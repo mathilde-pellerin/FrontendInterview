337b126334f2cc728cc8c030d7f15b66049e4042
Ricky Clegg <me@rickyclegg.com>
2015-07-15 17:24:34 +0100

    Test getting all grades on a 5door bodystyle

--- Feature/test/spec/ConfiguratorSpec.js
+++ Feature/test/spec/ConfiguratorSpec.js
@@ -15,4 +15,13 @@ describe('Configurator', function () {
         expect(grades[3]).toEqual('sport');
     });
 
+    it('should return me all available grades for bodyStyle 5door', function () {
+        var grades = configurator.getAvailableType('grade', '5door');
+
+        expect(grades.length).toEqual(3);
+        expect(grades[0]).toEqual('mid');
+        expect(grades[1]).toEqual('high');
+        expect(grades[2]).toEqual('sport');
+    });
+
 });
